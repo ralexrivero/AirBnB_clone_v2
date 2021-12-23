ee6156512a7083f334f469052f811e09a01158d6
ralexrivero <ralexrivero@gmail.com>
2021-12-22 21:21:15 -0300

    fix state

--- models/state.py
+++ models/state.py
@@ -3,6 +3,9 @@
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
+from os import getenv
+from models.city import City
+import models
 
 
 class State(BaseModel, Base):
@@ -11,3 +14,13 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship('City', cascade="delete",
                           backref='state')
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
