2a31f5a4a3a99c6a4598a229b68913a9209c68bf
ralexrivero <ralexrivero@gmail.com>
2021-12-23 22:38:52 -0300

    revert init.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """This module instantiates an storage"""
-from os import getenv
+from os import environ as env
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
+if env["HBNB_TYPE_STORAGE"] == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
+    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-storage.reload()
+    storage.reload()
