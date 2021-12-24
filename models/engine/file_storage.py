93810d76d50a96db46290ec1655adf3d59afdeb5
ralexrivero <ralexrivero@gmail.com>
2021-12-23 23:44:20 -0300

    update file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -41,12 +41,9 @@ class FileStorage:
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+        od = {o: self.__objects[o].to_dict() for o in self.__objects.keys()}
+        with open(self.__file_path, "w", encoding="utf-8") as f:
+            json.dump(od, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -59,16 +56,6 @@ class FileStorage:
         except FileNotFoundError:
             pass
 
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
-        try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
-            pass
+    def close(self):
+        """ reload """
+        self.reload()
