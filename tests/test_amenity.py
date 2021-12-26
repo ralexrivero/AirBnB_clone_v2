0ad39a0d0f78fc6c8cd6a461226a2428db5e01ad
ralexrivero <ralexrivero@gmail.com>
2021-12-26 11:53:46 -0300

    add test

---
+++ tests/test_amenity.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" """
+from tests.test_models.test_base_model import test_basemodel
+from models.amenity import Amenity
+
+
+class test_Amenity(test_basemodel):
+    """ test amenity """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "Amenity"
+        self.value = Amenity
+
+    def tname(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
